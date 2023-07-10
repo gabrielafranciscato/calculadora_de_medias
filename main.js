62a40c774212b6bd813ab1cdf1f48f26708c4cb4
Gabriela Franciscato <franciscato.gabriela@gmail.com>
2023-07-10 10:26:29 +0100

    insercao de alunos js

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images 3/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images 3/reprovado.png" alt="Emoji decepcionado" />';
+
+let linhas = '';
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado }</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+});
